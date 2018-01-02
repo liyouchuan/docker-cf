b10fbb70961ce5257cd8fab58156d1ca8303ff4e
ZxSeryNachuouy <34948261+ZxSeryNachuouy@users.noreply.github.com>
2018-01-02 13:36:16 -0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -2,7 +2,7 @@
 export DOCKER_HOST=tcp://192.168.99.100:2376
 export DOCKER_MACHINE_NAME=default
 export DOCKER_TLS_VERIFY=1
-export DOCKER_TOOLBOX_INSTALL_PATH=C:\Program Files\Docker Toolbox
-export DOCKER_CERT_PATH=C:\Users\ZxSeryNachuouy\.docker\machine\machines\default
+export DOCKER_TOOLBOX_INSTALL_PATH="/mnt/c/Program Files/Docker Toolbox"
+export DOCKER_CERT_PATH="/mnt/c/Users/ZxSeryNachuouy/.docker/machine/machines/default"
 #
 alias docker-machine='docker-machine.exe'
